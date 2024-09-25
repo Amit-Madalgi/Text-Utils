32573fdd609420ad40097c91c5c98d3cd1eb5abd
Amit57000 <166124764+Amit-Madalgi@users.noreply.github.com>
2024-09-25 20:33:23 +0530

    Fixed some bugs

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function App() {
       <div className="container my-3">
         <Routes>
           {/*Whatever components is inside <Routes> tag will change after routing*/}
-          <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}/>
+          <Route path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove Extra Spaces" mode={mode}/>}/>
           <Route path="/about" element={<About mode={mode} toggleMode={toggleMode}/>}/>
         </Routes>
       </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = () =>{
@@ -38,9 +38,7 @@ export default function TextForm(props) {
     }
 
     const handleCopy = (event) =>{
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        navigator.clipboard.writeText(text);
         props.showAlert("Copied text to clipboard", "success");
     }
 
@@ -50,25 +48,25 @@ export default function TextForm(props) {
   return (
         <>
         <div className='container'>
-            <h1>{props.heading}</h1>
+            <h1 className='mb-4'>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark' ? '#3e5975' : 'white',
                     color: props.mode === 'dark' ? 'white' : 'black'}}id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
-            <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to LowerCase</button>
-            <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleRemoveWhiteSpaceClick}>Remove Post White Spaces</button>
-            <button className="btn btn-primary mx-1" onClick={handleRemoveExtraSpace}>Remove Extra Spaces</button>
-            <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleLowClick}>Convert to LowerCase</button>
+            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleClearClick}>Clear Text</button>
+            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleRemoveWhiteSpaceClick}>Remove Post White Spaces</button>
+            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleRemoveExtraSpace}>Remove Extra Spaces</button>
+            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleCopy}>Copy Text</button>
         </div>  
 
         <div className="container my-3">
             <h2>Your Text Summary</h2>
-            <p>{text.split(" ").length-1} Words and {text.length} Characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Words and {text.length} Characters</p>
+            <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h2>Preview</h2>
-            <p>{text.length>0 ? text : "Enter something in the above textbox to preview it here"}</p>
+            <p>{text.length>0 ? text : "Nothing to Preview!"}</p>
         </div>
         </>
     )
